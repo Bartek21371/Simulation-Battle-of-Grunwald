38532f6f7d83708f3156e806552bc0b59ef0afd2
Bartek21371 <bartolino2005@gmail.com>
2026-05-21 22:06:55 +0200

    Update Tests (make more pro tests with Catch2, 3/4 test_config, test_combat, test_army)

--- tests/test_army.cpp
+++ tests/test_army.cpp
@@ -10,53 +10,34 @@
 
 // Test creating army
 
-//TEST_CASE("Test creating two armies") {
+TEST_CASE("Test creating two armies", "[army]") {
+    auto configs = ConfigLoader::LoadFromFile("../assets/configs/testfile.csv", false);
 
+    Army Hussars(configs[0]);
+    Army Teutonic(configs[1]);
 
-/*
-void test_army() {
-
-    std::vector<FractionConfig> config1 = ConfigLoader::LoadFromFile("../assets/configs/testfile.csv");
-
-    Army Hussars = Army(config1[0]);
-    Army Teutonic = Army(config1[1]);
-
-    // Check armies size
-    std::cout<<"Army size: "<<Hussars.get_ArmySize()<<"\n";
-    std::cout<<"Army size: "<<Teutonic.get_ArmySize()<<"\n";
-
-    std::cout<<"---------------------------------------------------------------"<<"\n";
-
-    // Check health of each warrior in army
-    std::cout<<"Health of hussars warriors:"<<"\n";
-    for (Warrior& w : Hussars.get_Warriors()) {
-        std::cout << w.get_Health() << "\n";
+    SECTION("Check sizes of armies") {
+        REQUIRE(Hussars.get_ArmySize()==135);
+        REQUIRE(Teutonic.get_ArmySize()==155);
     }
 
-    std::cout<<"Health of teutonic warriors:"<<"\n";
-    for (Warrior& w : Teutonic.get_Warriors()) {
-        std::cout << w.get_Health() << "\n";
+    SECTION("Check is all warriors is alive after create") {
+        REQUIRE(Hussars.count_AliveWarriors()==Hussars.get_ArmySize());
+        REQUIRE(Teutonic.count_AliveWarriors()==Teutonic.get_ArmySize());
     }
-    std::cout<<"---------------------------------------------------------------"<<"\n";
 
-    // Count alive warriors in armies after combat
-    std::cout<<"Before combat: "<<"\n";
-
-    std::cout<<"Alive hussars: "<<Hussars.count_AliveWarriors()<<"\n";
-    std::cout<<"Alive teutonic: "<<Teutonic.count_AliveWarriors()<<"\n";
-
-    std::cout<<"After combat: "<<"\n";
-
-    while (Teutonic.get_Warriors()[0].is_Alive()) {
-        Hussars.get_Warriors()[0].attack_Enemy(Teutonic.get_Warriors()[0]);
-        std::cout<<"Enemy health: "<<Teutonic.get_Warriors()[0].get_Health()<<"\n";
+    SECTION("Army contains warriors") {
+        REQUIRE_FALSE(Hussars.get_Warriors().empty());
+        REQUIRE_FALSE(Hussars.get_Warriors().empty());
     }
-    std::cout<<"Alive hussars: "<<Hussars.count_AliveWarriors()<<"\n";
-    std::cout<<"Alive teutonic: "<<Teutonic.count_AliveWarriors()<<"\n";
 
+    SECTION("All warriors have health greater than 0") {
+        for (auto& w : Hussars.get_Warriors()) {
+            REQUIRE(w.get_Health()>0);
+        }
+
+        for (auto& w : Teutonic.get_Warriors()) {
+            REQUIRE(w.get_Health()>0);
+        }
+    }
 }
-
-int main() {
-    test_army();
-}
-*/
